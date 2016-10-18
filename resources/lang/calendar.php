ab17f138d9698df3a604b37af8c167995c1ed757
Adnan <adnan@websemantics.ca>
2016-10-18 13:26:29 -0400

    Add view, less and language files

---
+++ resources/lang/calendar.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'days'          => [
+    'sunday' => 'Sunday',
+    'monday' => 'Monday',
+    'tuesday' => 'Tuesday',
+    'wednesday' => 'Wednesday',
+    'thursday' => 'Thursday',
+    'friday' => 'Friday',
+    'saturday' => 'Saturday'
+    ]
+];
---
+++ resources/lang/en/view.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+  'table'          => 'Table',
+  'calendar'          => 'Calendar'
+];
